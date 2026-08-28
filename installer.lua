8db1014d33cdcc1fc9291655b1881680d23df3f3
enzonp222 <enzo.novaes.palacio@gmail.com>
2026-08-28 18:53:30 +0000

    floppy

---
+++ installer.lua
@@ -0,0 +1,11 @@
+local BASE_URL =
+    "https://raw.githubusercontent.com/enzonp222/cclinux/refs/heads/main/"
+
+local arquivos = {
+    "startup.lua",
+    "floppy.lua",
+    "kernel.lua",
+    "login.lua",
+    "monitor.lua",
+    "shell.lua"
+}
